c0e318e26b8c4b0393f6785114d0b35be398d651
luishdzupiita <luisvh18@icloud.com>
2016-08-18 15:51:47 -0500

    Update arbol.js

--- rv/arbol.js
+++ rv/arbol.js
@@ -1,6 +1,7 @@
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 100;
+camara.position.y = 15;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
