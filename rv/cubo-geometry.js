fa15fbd38b71fa8b9971a3e1cf9ba7876caea846
luishdzupiita <luisvh18@icloud.com>
2016-08-25 15:12:51 -0500

    Update cubo-geometry.js

--- rv/cubo-geometry.js
+++ rv/cubo-geometry.js
@@ -33,7 +33,7 @@ malla.rotateX(Math.PI/4);
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 6;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
