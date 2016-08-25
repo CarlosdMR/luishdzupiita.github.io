e82d0e7fd297b91f653afe25d01a40721a4f5bb1
luishdzupiita <luisvh18@icloud.com>
2016-08-25 15:11:20 -0500

    Update cubo-geometry.js

--- rv/cubo-geometry.js
+++ rv/cubo-geometry.js
@@ -27,9 +27,8 @@ forma.computeFaceNormals();
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh(forma,material);
-malla.rotateY(Math.PI/3);
-malla.rotateZ(Math.PI/8);
-malla.rotateX(Math.PI/8);
+malla.rotateY(Math.PI/4);
+malla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add(malla);
