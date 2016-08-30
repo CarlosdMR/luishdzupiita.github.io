a5e96b1ca1dfad06b29d0ed352cf16a1823ccfac
luishdzupiita <luisvh18@icloud.com>
2016-08-30 15:23:21 -0500

    Update ejemplo-revolucion.js

--- rv/ejemplo-revolucion.js
+++ rv/ejemplo-revolucion.js
@@ -1,7 +1,7 @@
 var puntos = [];
 
 for (var i = 0; i<75; i++) {
-  puntos.push(new THREE.Vector2(Math.sin(i*0.13)*1+20,(i-25)*1));
+  puntos.push(new THREE.Vector2(Math.sin(i*0.13)*1+20,(i)*1));
 }
 
 var forma = new THREE.LatheGeometry(puntos);
@@ -12,7 +12,7 @@ var malla = new THREE.Mesh(forma,material);
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 200;
+camara.position.z = 400;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 
