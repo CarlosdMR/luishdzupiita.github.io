787a15cfcc1f1b72461938e2324bcd27dd982419
luishdzupiita <luisvh18@icloud.com>
2016-08-30 15:21:25 -0500

    Update ejemplo-extrusion.js

--- rv/ejemplo-extrusion.js
+++ rv/ejemplo-extrusion.js
@@ -14,7 +14,7 @@ malla.rotateX(Math.PI/4);
 var escena = new THREE.Scene();
 escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 500;
+camara.position.z = 200;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 
