df538d812832882cdc6aa2e4d093b7f1c5f3f8b4
luishdzupiita <luisvh18@icloud.com>
2016-09-01 15:55:29 -0500

    Create trabajo-estrella.js

---
+++ rv/trabajo-estrella.js
@@ -0,0 +1,28 @@
+var figura = new THREE.Shape();
+
+figura.moveTo(0,3);
+figura.lineTo(2,2);
+figura.lineTo(3,2);
+figura.lineTo(2,0);
+figura.lineTo(3,-2);
+figura.lineTo(1,-2);
+figura.lineTo(0,-3);
+figura.lineTo(-1,-2);
+figura.lineTo(-3,-2);
+figura.lineTo(-2,0);
+figura.lineTo(-3,2);
+figura.lineTo(-1,2);
+figura.lineTo(0,3);
+
+var forma = new THREE.ShapeGeometry(figura);
+var malla = new THREE.Mesh(forma);
+
+var escena = new THREE.Scene();
+escena.add(malla);
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
