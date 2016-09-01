898ea36f831a31e6d9f825d891003166f8e01569
luishdzupiita <luisvh18@icloud.com>
2016-09-01 14:58:42 -0500

    Update ejemplo-revolucion.js

--- rv/ejemplo-revolucion.js
+++ rv/ejemplo-revolucion.js
@@ -31,6 +31,5 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 400;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
-
 document.body.appendChild(renderizador.domElement);
 renderizador.render(scene,camara);
