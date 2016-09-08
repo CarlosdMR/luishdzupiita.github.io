5eeda55d4c77f1589ff1b48b9c79c07e2a3a2580
luishdzupiita <luisvh18@icloud.com>
2016-09-08 15:08:00 -0500

    Update ejercicioTablero1.js

--- rv/ejercicioTablero1.js
+++ rv/ejercicioTablero1.js
@@ -5,20 +5,52 @@ var planoLejano = 1000;
 
 
 var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
-camara.position.z = 15;
+//camara.position.x = 100;
+//camara.position.y = 100;
+camara.position.z = 100;
 
-var colorNegro = new THREE.Color("rgb(0, 0, 0)");
-var materialNegro = new THREE.MeshBasicMaterial();
-materialNegro.color = colorNegro;
+var colorGris = new THREE.Color("rgb(30, 30, 30)");
+var materialGris = new THREE.MeshBasicMaterial();
+materialGris.color = colorGris;
 var colorBlanco = new THREE.Color("rgb(255, 255, 255)");
 var materialBlanco = new THREE.MeshBasicMaterial();
 materialBlanco.color = colorBlanco;
 
+function isEven(n) {
+   return n % 2 == 0;
+}
 
+function isOdd(n) {
+   return Math.abs(n % 2) == 1;
+}
+
+var cuadros = [];
+var cubeSize = 10;
+var intialMaterial = colorGris;
+for (i = 0; i < 8; i++) {
+  for (j = 0; j < 8; j++) {
+    var material = colorGris;
+    if (isEven(i)) {
+      if (isOdd(j)) {
+        material = colorBlanco;
+      } 
+    } else {
+      if (isEven(j)) {
+        material = colorBlanco;
+      } 
+    }
+    var cubo = new THREE.Mesh(new THREE.BoxGeometry(cubeSize,cubeSize,cubeSize),material);
+    cubo.position.x = j*cubeSize;
+    cubo.poistion.y = i*cubeSize;
+    cuadros.push(cubo);
+  }
+}
 
 //////////////////////////////////////////////////////////////////
 var escena = new THREE.Scene();
-escena.add(esfera1,esfera2,cubo,light,spotLight);
+for cube in cuadros {
+escena.add(cube);
+}
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
