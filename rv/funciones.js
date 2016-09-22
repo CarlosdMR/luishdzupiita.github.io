cc7155bc9323c53a73e093a891fbbcc505f2960f
luishdzupiita <luisvh18@icloud.com>
2016-09-22 15:25:03 -0500

    Update funciones.js

--- rv/funciones.js
+++ rv/funciones.js
@@ -1,9 +1,9 @@
-var escena, camara, renderizador;
+var escena, camara, renderizador, malla;
 
 function init(p) {
 
 var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh( new THREE.BoxGeometry(p,p,p), material);
+malla = new THREE.Mesh( new THREE.BoxGeometry(p,p,p), material);
 
 escena = new THREE.Scene();
 escena.add(malla);
@@ -12,13 +12,15 @@ camara = new THREE.PerspectiveCamera();
 camara.position.z = 3*p;
 
 renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(100,700);
+renderizador.setSize(700,700);
 document.body.appendChild(renderizador.domElement);
 
 }
- var main = function(p) {
-   p(1);
+ var loop = function() {
+   requestAnimatioFrame(loop);
    renderizador.render(escena,camara);
+   malla.rotateY(0.01);
  }
  
- main(init);
+ init(1);
+ loop();
