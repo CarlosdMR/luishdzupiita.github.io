2d40df06054a9691af67dacd42dffa139e9126e9
CarlosdMR <carlosd.marceleno@gmail.com>
2017-05-25 11:57:37 -0500

    Update camaraOrtografica.js

--- rv/camaraOrtografica.js
+++ rv/camaraOrtografica.js
@@ -4,7 +4,7 @@ camara.left = window.innerWidth / -2;
 camara.right = window.innerWidth / 2;
 camara.top = window.innerHeight / 2;
 camara.bottom = window.innerHeight / -2;
-camara.near = 50;
+camara.near = 0.1;
 camara.far = 1000;
 camara.updateProjectionMatrix();
 
